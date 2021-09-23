5eff2f2bb2146827ebc4e5a4f131438f59ba915b
Georgeluobe007 <68495623+Georgeluobe007@users.noreply.github.com>
2021-09-23 07:15:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <div className="grid-component">
+         <header>
+           <a href="/" >React Shopping Cart</a>
+         </header>
+         <main>
+           Product List
+         </main>
+         <footer>
+           All right reserved
+         </footer>
+     </div>
+   </div>
   );
 }
 
