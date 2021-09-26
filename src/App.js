ff774bfe7062b9ca9cc45b4a259a6890ae5b189f
Georgeluobe007 <georgedaninno@gmail.co>
2021-09-26 10:50:42 -0700

    cart components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
+import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Data from './Data.json'
+import Product from './components/Product';
+export default class App extends Component {
 
+  constructor(){
+    super();
+    this.state={
+      Products : Data.ContinentalProduct,
+      sort:"",
+      size: "",
+    }
+  }
+  render() {
+    return (
+      <div className="grid-layout">
+        <header>
+          <a href="/">React Shoping Cart</a>
+        </header>
 
-function App() {
-  return (
-   <div>
-     <div className="grid-component">
-         <header>
-           <a href="/" >React Shopping Cart</a>
-         </header>
-         <main>
-           Product List
-         </main>
-         <footer>
-           All right reserved
-         </footer>
-     </div>
-   </div>
-  );
+        <main>
+         <div className="content">
+<div className="main">
+  <Product product={this.state.Products}/>
+</div>
+      <div className="side-bar">
+      Carts Items
+      </div>
+         </div>
+        </main>
+        <footer>
+          All right researve
+        </footer>
+      </div>
+    )
+  }
 }
-
-export default App;
